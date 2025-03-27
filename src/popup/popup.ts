4654c1db907a34e777c533b58a7121642f5331ba
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2025-03-27 18:13:32 +0900

    first commit

---
+++ src/popup/popup.ts
@@ -0,0 +1,23 @@
+// ↓ は後で消す
+console.log("file_name : popup.ts or popup.js");
+
+document.addEventListener("click", (e) => {
+    const target = e.target as HTMLElement;
+    if (target && target.id === "start-btn") {
+        target.id = "reset-btn";
+        target.textContent = "リセット";
+
+        const today = new Date();
+        const hour = today.getHours();
+        const minute = today.getMinutes();
+        const second = today.getSeconds();
+
+        const current_time = hour + "時" + minute + "分" + second + "秒";
+        console.log("time: " + current_time);
+    } else if (target && target.id === "reset-btn") {
+        target.id = "start-btn";
+        target.textContent = "スタート";
+    } else {
+        console.log("nothing process");
+    }
+});
