93cb38fb9928614cee6061721db252f28195e129
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2025-03-27 22:49:41 +0900

    Jest による TDD が行える環境を用意した

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,23 +1,27 @@
 // ↓ は後で消す
 console.log("file_name : popup.ts or popup.js");
 
-document.addEventListener("click", (e) => {
-    const target = e.target as HTMLElement;
-    if (target && target.id === "start-btn") {
-        target.id = "reset-btn";
-        target.textContent = "リセット";
+if (typeof document === "undefined") {
+    console.log("document is undefined");
+} else {
+    document.addEventListener("click", (e: MouseEvent) => {
+        const target = e.target as HTMLElement;
+        if (target && target.id === "start-btn") {
+            target.id = "reset-btn";
+            target.textContent = "リセット";
 
-        const today = new Date();
-        const hour = today.getHours();
-        const minute = today.getMinutes();
-        const second = today.getSeconds();
+            const today = new Date();
+            const hour = today.getHours();
+            const minute = today.getMinutes();
+            const second = today.getSeconds();
 
-        const current_time = hour + "時" + minute + "分" + second + "秒";
-        console.log("time: " + current_time);
-    } else if (target && target.id === "reset-btn") {
-        target.id = "start-btn";
-        target.textContent = "スタート";
-    } else {
-        console.log("nothing process");
-    }
-});
+            const current_time = hour + "時" + minute + "分" + second + "秒";
+            console.log("time: " + current_time);
+        } else if (target && target.id === "reset-btn") {
+            target.id = "start-btn";
+            target.textContent = "スタート";
+        } else {
+            console.log("nothing process");
+        }
+    });
+}
