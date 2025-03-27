b5ed231bb5bba5f103e6c0cdad2dd073d8112cb4
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2025-03-27 23:44:10 +0900

    リファクタリング

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -6,23 +6,25 @@ if (typeof document === "undefined") {
 }
 else {
     document.addEventListener("click", (e) => {
-        const target = e.target;
-        if (target && target.id === "start-btn") {
-            target.id = "reset-btn";
-            target.textContent = "リセット";
-            const today = new Date();
-            const hour = today.getHours();
-            const minute = today.getMinutes();
-            const second = today.getSeconds();
-            const current_time = hour + "時" + minute + "分" + second + "秒";
-            console.log("time: " + current_time);
-        }
-        else if (target && target.id === "reset-btn") {
-            target.id = "start-btn";
-            target.textContent = "スタート";
-        }
-        else {
-            console.log("nothing process");
+        const btn = e.target;
+        switch (btn?.id) {
+            case "start-btn":
+                btn.id = "reset-btn";
+                btn.textContent = "リセット";
+                const today = new Date();
+                const hour = today.getHours();
+                const minute = today.getMinutes();
+                const second = today.getSeconds();
+                const current_time = hour + "時" + minute + "分" + second + "秒";
+                console.log("time: " + current_time);
+                break;
+            case "reset-btn":
+                btn.id = "start-btn";
+                btn.textContent = "スタート";
+                break;
+            default:
+                console.log("nothing process");
+                break;
         }
     });
 }
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -5,23 +5,30 @@ if (typeof document === "undefined") {
     console.log("document is undefined");
 } else {
     document.addEventListener("click", (e: MouseEvent) => {
-        const target = e.target as HTMLElement;
-        if (target && target.id === "start-btn") {
-            target.id = "reset-btn";
-            target.textContent = "リセット";
+        const btn = e.target as HTMLButtonElement;
 
-            const today = new Date();
-            const hour = today.getHours();
-            const minute = today.getMinutes();
-            const second = today.getSeconds();
+        switch (btn?.id) {
+            case "start-btn":
+                btn.id = "reset-btn";
+                btn.textContent = "リセット";
 
-            const current_time = hour + "時" + minute + "分" + second + "秒";
-            console.log("time: " + current_time);
-        } else if (target && target.id === "reset-btn") {
-            target.id = "start-btn";
-            target.textContent = "スタート";
-        } else {
-            console.log("nothing process");
+                const today = new Date();
+                const hour = today.getHours();
+                const minute = today.getMinutes();
+                const second = today.getSeconds();
+
+                const current_time = hour + "時" + minute + "分" + second + "秒";
+                console.log("time: " + current_time);
+                break;
+
+            case "reset-btn":
+                btn.id = "start-btn";
+                btn.textContent = "スタート";
+                break;
+
+            default:
+                console.log("nothing process");
+                break;
         }
     });
 }
