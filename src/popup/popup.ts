273e7a2be62accd47b49e90cf9e49a91a3ff0be6
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2025-03-31 15:22:54 +0900

    定数（文字列に限る）として頻出するものはタイポを防ぐためにグローバル変数で定義
    storage の key に関しては可読性が悪くなりそうなので、グローバル変数で定義しない

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,6 +1,22 @@
+const START_BTN_ID: string = "start-btn";
+const RESET_BTN_ID: string = "reset-btn";
+const SAVE_TODO_BTN_ID: string = "save-todo-btn";
+const CLEAR_TODO_BTN_ID: string = "clear-todo-btn";
+
+const TODO_TEXTAREA_ID: string = "todo";
+const TIMER_ID: string = "timer";
+const TASK_ID: string = "task";
+
+const TASK_WORK: string = "work";
+const TASK_BREAK: string = "break";
+const TASK_NOTHING: string = "nothing";
+
+const COUNTDOWN_STATE: string = "countDown";
+const WAIT_STATE: string = "wait";
+
 // 拡張機能のポップアップを開いたときに、保存されている todo があればそれを textarea に表示する
 chrome.storage.local.get(["todoData"], function (storage) {
-    setTextContentById("todo", storage.todoData);
+    setTextContentById(TODO_TEXTAREA_ID, storage.todoData);
 });
 
 // 拡張機能のポップアップを開いたときに、保存されている残り時間と作業内容があればそれらを表示させる
@@ -8,15 +24,15 @@ chrome.storage.local.get(["state", "task", "minute", "second"], function (storag
     setMinuteAndSecondDisplay(storage.minute, storage.second);
 
     const taskTextContent = getTaskTextContent(storage.task);
-    setTextContentById("task", taskTextContent);
+    setTextContentById(TASK_ID, taskTextContent);
 
     switch (storage.state) {
-        case "countDown":
-            setButtonTextContentAndIdNameById("start-btn", "reset-btn");
+        case COUNTDOWN_STATE:
+            setButtonTextContentAndIdNameById(START_BTN_ID, RESET_BTN_ID);
             break;
 
         default:
-            setButtonTextContentAndIdNameById("reset-btn", "start-btn");
+            setButtonTextContentAndIdNameById(RESET_BTN_ID, START_BTN_ID);
             break;
     }
 });
@@ -24,19 +40,19 @@ chrome.storage.local.get(["state", "task", "minute", "second"], function (storag
 // カウントダウン処理
 setInterval(() => {
     chrome.storage.local.get(["state", "task", "minute", "second"], function (storage) {
-        if (storage.state === "countDown") {
+        if (storage.state === COUNTDOWN_STATE) {
             const minute = storage.minute;
             const second = storage.second;
             const restSecond = 60 * minute + second;
             const nextSecond = restSecond - 1;
 
             if (nextSecond === -1) {
-                if (storage.task === "work") {
+                if (storage.task === TASK_WORK) {
                     alert("休憩時間に入ります。");
-                    chrome.storage.local.set({ "task": "break", "minute": 5, "second": 0 });
-                } else if (storage.task === "break") {
+                    chrome.storage.local.set({ "task": TASK_BREAK, "minute": 5, "second": 0 });
+                } else if (storage.task === TASK_BREAK) {
                     alert("作業時間に入ります。");
-                    chrome.storage.local.set({ "task": "work", "minute": 25, "second": 0 });
+                    chrome.storage.local.set({ "task": TASK_WORK, "minute": 25, "second": 0 });
                 }
             } else {
                 const newMinute = Math.floor(nextSecond / 60);
@@ -52,12 +68,12 @@ document.addEventListener("click", (e: MouseEvent) => {
     const btnElement = e.target as HTMLButtonElement;
 
     switch (btnElement?.id) {
-        case "start-btn":
-            setButtonTextContentAndIdNameById(btnElement.id, "reset-btn");
+        case START_BTN_ID:
+            setButtonTextContentAndIdNameById(START_BTN_ID, RESET_BTN_ID);
             chrome.storage.local.set(
                 {
-                    "state": "countDown",
-                    "task": "work",
+                    "state": COUNTDOWN_STATE,
+                    "task": TASK_WORK,
                     "minute": 25,
                     "second": 0,
                 },
@@ -65,12 +81,12 @@ document.addEventListener("click", (e: MouseEvent) => {
             );
             break;
 
-        case "reset-btn":
-            setButtonTextContentAndIdNameById(btnElement.id, "start-btn");
+        case RESET_BTN_ID:
+            setButtonTextContentAndIdNameById(RESET_BTN_ID, START_BTN_ID);
             chrome.storage.local.set(
                 {
-                    "state": "wait",
-                    "task": "nothing",
+                    "state": WAIT_STATE,
+                    "task": TASK_NOTHING,
                     "minute": 25,
                     "second": 0,
                 },
@@ -78,11 +94,11 @@ document.addEventListener("click", (e: MouseEvent) => {
             );
             break;
 
-        case "save-todo-btn":
+        case SAVE_TODO_BTN_ID:
             chrome.runtime.sendMessage(
                 {
                     action: "click-save-todo-btn",
-                    todoData: (document.getElementById("todo") as HTMLInputElement).value
+                    todoData: (document.getElementById(TODO_TEXTAREA_ID) as HTMLInputElement).value
                 },
                 function (response) {
                     alert(response.replyMessage);
@@ -90,9 +106,9 @@ document.addEventListener("click", (e: MouseEvent) => {
             );
             break;
 
-        case "clear-todo-btn":
+        case CLEAR_TODO_BTN_ID:
             const todoTextContent: string = "";
-            setValueById("todo", todoTextContent); // 保存せずに入力された todo もクリアする
+            setValueById(TODO_TEXTAREA_ID, todoTextContent); // 保存せずに入力された todo もクリアする
 
             chrome.runtime.sendMessage(
                 {
@@ -119,7 +135,7 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
 
     // todo の更新
     if ("todoData" in changes) {
-        setTextContentById("todo", changes.todoData.newValue);
+        setTextContentById(TODO_TEXTAREA_ID, changes.todoData.newValue);
     }
 
     // 残り時間の更新
@@ -132,16 +148,16 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
     // 作業内容の更新
     if ("task" in changes) {
         const taskTextContent = getTaskTextContent(changes.task.newValue);
-        setTextContentById("task", taskTextContent);
+        setTextContentById(TASK_ID, taskTextContent);
     }
 });
 
 // テキストコンテンツに格納するタスクの内容を取得する関数
 function getTaskTextContent(task: string): string {
     switch (task) {
-        case "work":
+        case TASK_WORK:
             return "TOMATODO（作業中）";
-        case "break":
+        case TASK_BREAK:
             return "TOMATODO（休憩中）";
         default:
             return "TOMATODO（待機中）";
@@ -181,9 +197,9 @@ function setIdById(targetElementName: string, newId: string): void {
 // 変更後の id 名を引数として受け取り、変更後のボタン名を取得する関数
 function getNewButtonName(newIdName: string): string {
     switch (newIdName) {
-        case "start-btn":
+        case START_BTN_ID:
             return "スタート";
-        case "reset-btn":
+        case RESET_BTN_ID:
             return "リセット";
         default:
             console.error(`要素が見つかりませんでした。要素名: ${newIdName}`);
@@ -204,7 +220,7 @@ function setMinuteAndSecondDisplay(minute: number, second: number): void {
         const displayMinute = String(minute).padStart(2, "0");
         const displaySecond = String(second).padStart(2, "0");
         const newTextContent = `${displayMinute}:${displaySecond}`;
-        setTextContentById("timer", newTextContent);
+        setTextContentById(TIMER_ID, newTextContent);
     } else {
         console.error(`分または秒の値が未定義です。minute: ${minute}, second: ${second}`);
     }
